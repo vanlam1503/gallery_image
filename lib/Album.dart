3fb2203f0e9738980f3b7e26133dc896463aa9cd
vanlam <vanlam1503@gmail.com>
2021-11-07 15:21:48 +0700

    Add android internet permission

--- lib/Album.dart
+++ lib/Album.dart
@@ -18,14 +18,11 @@ class Album {
 Future<Album> fetchAlbum() async {
   final response = await http
       .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+  print('vanlam');
+  print(response);
   if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-
     return Album.fromJson(jsonDecode(response.body));
   } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
     throw Exception('Failed to load album');
   }
 }
